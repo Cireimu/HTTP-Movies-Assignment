537a7237284487dc26e51be92fb89461c7f99814
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2019-11-14 16:40:12 -0800

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
+  
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movieToEdit, setMovieToEdit] = useState({});
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+
+  const updateMovie = movie => {
+    setMovieToEdit(movie);
+  };
+
+  console.log(movieToEdit);
 
   return (
     <>
@@ -18,7 +27,19 @@ const App = () => {
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return (
+            <Movie
+              {...props}
+              addToSavedList={addToSavedList}
+              updateMovie={updateMovie}
+            />
+          );
+        }}
+      />
+      <Route
+        path="/update-movie/:id"
+        render={props => {
+          return <UpdateMovie {...props} movie={movieToEdit} />;
         }}
       />
     </>
